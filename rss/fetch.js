c48149c99536c8e19ed4aadcdc50839195152298
taplinseth <taplinseth@gmail.com>
2022-11-17 19:58:57 -0600

    converted rss feed into json file

--- rss/fetch.js
+++ rss/fetch.js
@@ -1,13 +1,25 @@
 let Parser = require('rss-parser');
+let fs = require('fs-extra');
 
-(async function getFeed() {
-    
+module.exports = async function getFeed() {
+    let items = [];
     let parser = new Parser();
 
     let feed = await parser.parseURL('https://rss.indeed.com/rss?q=javascript&l=&from=searchOnHP&vjk=352c2bbcdeb4ef2f');
-    console.log(feed.title);
+    
+    const fileName = `${feed.title.replace(/\s+/g, "_").replace(/[/\\?%*|"<>]/g, '').toLowerCase()}.json`
 
-    feed.items.forEach(item => {
-        console.log(item.title + ':' + item.link)
-    });
-})();
+    await Promise.all(feed.items.map(async (currentItem) => {
+        if (items.filter((item) => item === currentItem).length <= 1) {
+            items.push(currentItem);
+        }
+    }));
+
+    fs.writeFileSync(fileName, JSON.stringify(items));
+
+    // feed.items.forEach(item => {
+    //     console.log(item.title + ':' + item.link)
+    // });
+};
+
+getFeed();
---
+++ rss/json_parser.js
@@ -0,0 +1,4 @@
+let jobData = JSON.parse('./javascript_jobs__indeed.com.json');
+alert(jobData[0].title)
+
+console.log(jobData);

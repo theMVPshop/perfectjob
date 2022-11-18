8a1739bae4db429ca5b30906844f2525726a147e
RamonCruz-Hinojosa <Bairdwins@gmail.com>
2022-11-17 19:43:29 -0600

    map api working, small css snippet added to make map visible on page

--- geomapper/geomap.js
+++ geomapper/geomap.js
@@ -1,1 +1,11 @@
 // geomap file first push
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+window.initMap = initMap;

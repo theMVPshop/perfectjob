f5344b6c9db1438a1aeea57698b266757918ae74
Jank Majesty <jank@janks-air.lan>
2022-11-28 19:57:24 -0600

    searchbar2

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,11 +3,10 @@ import { styled, alpha } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
 import InputBase from '@mui/material/InputBase';
-import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
+
 
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
@@ -23,6 +22,16 @@ const Search = styled('div')(({ theme }) => ({
     width: 'auto',
   },
 }));
+
+
+const commonStyles = {
+  bgcolor: 'background.paper',
+  borderColor: 'text.primary',
+  m: 1,
+  border: 1,
+  width: '5rem',
+  height: '5rem',
+};
 
 const SearchIconWrapper = styled('div')(({ theme }) => ({
   padding: theme.spacing(0, 2),
@@ -49,7 +58,7 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   '& .MuiInputBase-input': {
     padding: theme.spacing(1, 1, 1, 0),
     // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    paddingLeft: `calc(1em + ${theme.spacing(0)})`,
     transition: theme.transitions.create('width'),
     width: '100%',
     [theme.breakpoints.up('sm')]: {
@@ -63,18 +72,13 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 
 export default function SearchAppBar() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" style={{ background: '#FFFFFF' }}>
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
+    <Box sx={{ flexGrow: 1, boxShadow: 2, width: "50%", position:'center' }}>
+      <AppBar position="relative" style={{ background: '#FFFFFF' }}>
+        <Toolbar
+          sx={{
+            width: "200",
+            maxWidth: 200
+          }}>
       
           <Search>
             <SearchIconWrapper>

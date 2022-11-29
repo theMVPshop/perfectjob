921799394be549a50029f1d5a15179a212e355e6
taplinseth <taplinseth@gmail.com>
2022-11-28 18:55:13 -0600

    parse rss feed

--- rss/json_parser.js
+++ rss/json_parser.js
@@ -1,10 +1,27 @@
 const fs = require('fs-extra');
 
+/*This will search the jobData.json file. I think this actually needs to be on the react side
+in order to show the results on the webpage after the search has completed. So just convert this 
+over to something that will work in React and use it there.*/
+
+
+//reads jobData json file 
 fs.readFile('./jobData.json', (err, data) => {
     if (err)
       console.log(err);
     else {
-      var json = JSON.parse(data);
-        console.log(json.title)
+      var json = JSON.parse(data); //saves json object to variable
+        // console.log(json)
+    }
+
+    //variables for title, link, and content snippet from json object
+    for(const property in json) {
+        let title = json[property].title;
+        let link = json[property].link;
+        let contentSnippet = json[property].contentSnippet;
+
+        // console.log(title);
+        // console.log(link);
+        // console.log(contentSnippet);
     }
 })

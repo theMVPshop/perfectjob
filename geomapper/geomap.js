7d7732b255f54d1b0bdfd7dcd4fdffc494a64bf4
RamonCruz-Hinojosa <Bairdwins@gmail.com>
2022-11-29 19:51:46 -0600

    leaflet replaced google maps, map is working

--- geomapper/geomap.js
+++ geomapper/geomap.js
@@ -1,12 +1,17 @@
-// geomap file first push
-import GEO_API_KEY from .env;
-let map;
+alert("success");
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
-}
+var map = L.map("map").setView([30.2019024431465, -97.87393387393803], 13);
+// creates the box that holds the map tiles
 
-window.initMap = initMap;
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+// fills in the map with map tiles, without this the map is just grey
+
+var marker = L.marker([30.2019024431465, -97.87393387393803]).addTo(map);
+// creates a marker on the specified coordiantes, above code will be used to auto mark the specified job
+
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+// creates a popup above markers, maybe pull job name from rss feed to fill this pop up

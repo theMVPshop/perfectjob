db9db7b31be478fdb58a241b249e5fd481c3ff90
RamonCruz-Hinojosa <bairdwins@gmail.com>
2022-12-19 20:03:16 -0600

    adding map button to search results

--- fetch.js
+++ fetch.js
@@ -1,3 +1,4 @@
+
 let jobTitle = '';
 let jobLocation = '';
 let positionType = '';
@@ -31,20 +32,22 @@ async function getFeed() {
 
   parser.parseURL(`https://rss.indeed.com/rss/jobs?q=${jobTitle}&l=${jobLocation}&sc=0kf%3Ajt%28${positionType}%29%3B&radius=0&vjk=2dd88a57cffe14c8`, function(err, feed) {
       if (err) throw err;
-      console.log(feed.title);
+      console.log(feed);
       console.log(positionType)
 
       feed.items.forEach(function(entry) {
-      
+        console.log(entry)
           let h2 = document.createElement('h1');
           let h3 = document.createElement('h2');
           let p = document.createElement('p');
+          let btn = document.createElement('button')
+          btn.innerHTML = "Find Job (This will open a popup!)"
 
           let li = document.getElementById('jobData');
-          let text = document.createTextNode(entry.title + '\n' + entry.link + '\n' + entry.contentSnippet);
+          let text = document.createTextNode(entry.title + '\n' + entry.contentSnippet);
           li.appendChild(text);
           document.body.appendChild(li)
-          console.log(entry.title + ':' + entry.link);
+        //   console.log(entry.title + ':' + entry.link);
       })
   })
 };

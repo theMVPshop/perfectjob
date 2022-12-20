59d45192e42d94aca57d908f9b8f3bd05267b9d2
RamonCruz-Hinojosa <bairdwins@gmail.com>
2022-12-19 19:24:34 -0600

    cleanup geomap

--- fetch.js
+++ fetch.js
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-async function getFeed(event, jobTitle, location, positionType) {
-    // preventDefault()
-    let parser = new RSSParser({
-        customFields: {
-            item: ['georss:point', 'geo']
-        }
-    });
-    console.log(event)
-
-    // custom field pulls georss data
-    
-    //actual values need to be equal to front end user inputs
-    // let jobTitle = document.getElementById('jobTitle');
-    // let location = document.getElementById('location');
-    // let positionType = document.getElementById('positionType');
-
-    parser.parseURL(`https://www.indeed.com/rss`, function(err, feed) {
-        if (err) throw err;
-        console.log(feed);
-
-        feed.items.forEach(function(entry) {
-        
-            let h2 = document.createElement('h1');
-            let h3 = document.createElement('h2');
-            let p = document.createElement('p');
-
-            let li = document.getElementById('jobData');
-            let text = document.createTextNode(entry.title + '\n' + entry.link + '\n' + entry.contentSnippet);
-            li.appendChild(text);
-            document.body.appendChild(li)
-            console.log(entry.title + ':' + entry.link);
-        })
-    })
-};
-=======
 
 let jobTitle = '';
 let jobLocation = '';
@@ -85,4 +49,3 @@ async function getFeed() {
       })
   })
 };
->>>>>>> main

cc3e740fdacbab8d66924547c76e27635dafeeb1
Jank <lmcgill36@gmail.com>
2023-01-05 19:34:03 -0600

    adding state to city autocomplete

--- citiesautocomplete.js
+++ citiesautocomplete.js
@@ -7,7 +7,7 @@ fetch('cities.json')
     // Loop through the cities array and create an option element for each city
     cities.forEach(city => {
       const option = document.createElement('option');
-      option.value = city.name;
+      option.value = `${city.name}, ${city.usps}`;
       datalist.appendChild(option);
     });
   });
